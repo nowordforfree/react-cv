c3a19b1513f6edd299b5bbff67aafde40fcaeea9
nowordforfree <nowordforfree@gmail.com>
2017-02-14 13:17:22 +0200

    reduced 404 component code

--- public/js/components/Error404/index.js
+++ public/js/components/Error404/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
-export default (props) => {
-    return (
-        <div className="alert alert-danger text-center">
-            <h1>404</h1>
-            Oops. Page not found
-        </div>
-    );
-};
+export default () => (
+  <div className="alert alert-danger text-center">
+    <h1>404</h1>
+    Oops. Page not found
+  </div>
+);
