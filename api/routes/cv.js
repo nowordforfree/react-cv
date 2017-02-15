278359d7eab6d4e2fd02a4e96b727d7c105dfc6e
nowordforfree <nowordforfree@gmail.com>
2017-02-15 11:41:25 +0200

    Updated PUT route to delete removed nested objects

--- api/routes/cv.js
+++ api/routes/cv.js
@@ -2,6 +2,18 @@ const express = require('express');
 const db      = require('../models');
 const router  = express.Router();
 
+function getMissingIds(source, target) {
+  const existing = target.filter(obj => obj.id && obj.CvId);
+  const result = source.map((a) => {
+    if (existing.some(b => a.id === b.id)) {
+      return;
+    } else {
+      return a.id;
+    }
+  }).filter(obj => obj);
+  return result;
+}
+
 router.get('/', (req, res) => {
   db
     .Cv
@@ -80,34 +92,64 @@ router.put('/:id', (req, res) => {
       db
         .sequelize
         .transaction((t) => {
-          return cv.update(req.body, {
-            transaction: t
+          let result;
+          const experiencesToDelete = getMissingIds(
+            cv.experiences, (req.body.experiences || [])
+          );
+          if (experiencesToDelete.length) {
+            result = db.Experience.destroy({
+              where: { id: experiencesToDelete },
+              transaction: t
+            });
+          } else {
+            result = Promise.resolve();
+          }
+          return result.then(() => {
+            const projectsToDelete = getMissingIds(
+              cv.projects,
+              (req.body.projects || [])
+            );
+            if (projectsToDelete.length) {
+              return db.Project.destroy({
+                where: { id: projectsToDelete },
+                transaction: t
+              });
+            } else {
+              return Promise.resolve();
+            }
           })
-          .then((cv) => {
-            let promises = [];
-            if (req.body.projects) {
+          .then(() => {
+            let projectsToAdd = [];
+            if (req.body.projects &&
+                req.body.projects.length) {
               req.body.projects.forEach(project => {
                 if (!project.CvId) {
                   Object.assign(project, { CvId: cv.get('id') });
                 }
-                promises.push(db.Project.upsert(project, {
+                projectsToAdd.push(db.Project.upsert(project, {
                   transaction: t
                 }));
               });
             }
-            if (req.body.experiences) {
+            return Promise.all(projectsToAdd);
+          })
+          .then(() => {
+            let experiencesToAdd = [];
+            if (req.body.experiences &&
+                req.body.experiences.length) {
               req.body.experiences.forEach(experience => {
                 if (!experience.CvId) {
                   Object.assign(experience, { CvId: cv.get('id') });
                 }
-                promises.push(db.Experience.upsert(experience, {
+                experiencesToAdd.push(db.Experience.upsert(experience, {
                   transaction: t
                 }));
               })
             }
-            return Promise.all(promises);
+            return Promise.all(experiencesToAdd);
           })
-          .then(() => cv);
+          .then(() => cv.update(req.body, { transaction: t }))
+          .then(cv => cv)
         })
         .then(cv => res.json({ data: cv }))
         .catch(err => res.status(500).json({ error: err }));
